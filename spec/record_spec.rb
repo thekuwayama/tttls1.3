51fe4a58a4fd43dfad1977b09c055924993d7567
thekuwayama <thekuwayama@gmail.com>
2019-02-08 18:57:27 +0900

    Add testcase of Record

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -19,4 +19,37 @@ RSpec.describe Record do
       expect(record.serialize).to eq [0x16, 0x03, 0x03, 0x00, 0x00]
     end
   end
+
+  context 'valid record header binary' do
+    let(:record) do
+      Record.deserialize(RECORD_HEADER, Passer.new)
+    end
+
+    it 'should generate valid record' do
+      expect(record.type).to eq ContentType::HANDSHAKE
+      expect(record.legacy_record_version).to eq ProtocolVersion::TLS_1_2
+      expect(record.length).to eq 0
+    end
+  end
+
+  context 'invalid record header binary, too short' do
+    let(:record) do
+      Record.deserialize(RECORD_HEADER[0...RECORD_HEADER.length - 1],
+                         Passer.new)
+    end
+
+    it 'should not generate record' do
+      expect { record }.to raise_error(RuntimeError)
+    end
+  end
+
+  context 'invalid record header binary, binary is nil' do
+    let(:record) do
+      Record.deserialize(nil, Passer.new)
+    end
+
+    it 'should not generate record' do
+      expect { record }.to raise_error(RuntimeError)
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,9 @@ RSpec.configure(&:disable_monkey_patching!)
 require 'tls13'
 include TLS13
 include TLS13::Message
+include TLS13::Cryptograph
 # rubocop: enable Style/MixinUsage
+
+RECORD_HEADER = '16 03 03 00 00'.split.map(&:hex).freeze
+
+# https://tools.ietf.org/html/rfc8448#page-3
