4ab5e68f45ffd6a48c1ab0874580f3353540506e
thekuwayama <thekuwayama@gmail.com>
2019-02-08 22:34:16 +0900

    Add comment to Passer

--- lib/tls13/cryptograph/passer.rb
+++ lib/tls13/cryptograph/passer.rb
@@ -1,10 +1,12 @@
 module TLS13
   module Cryptograph
     class Passer
+      # @param plaintext [Array of Integer]
       def encrypt(plaintext)
         plaintext
       end
 
+      # @param ciphertext [Array of Integer]
       def decrypt(ciphertext)
         ciphertext
       end
