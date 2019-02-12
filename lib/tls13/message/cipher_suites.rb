273e5246640a285de43c336227032cf1e7954bc7
thekuwayama <thekuwayama@gmail.com>
2019-02-13 01:37:13 +0900

    Implement CipherSuites.serialize()

--- lib/tls13/message/cipher_suites.rb
+++ lib/tls13/message/cipher_suites.rb
@@ -8,20 +8,25 @@ module TLS13
       TLS_AES_128_CCM_8_SHA256     = [0x13, 0x05].freeze
     end
 
+    DEFALT_CIPHER_SUITES = [CipherSuite::TLS_AES_256_GCM_SHA384,
+                            CipherSuite::TLS_CHACHA20_POLY1305_SHA256,
+                            CipherSuite::TLS_AES_128_GCM_SHA256].freeze
+
     class CipherSuites
       attr_accessor :length
       attr_accessor :cipher_suites
 
-      def initialize(cipher_suites: [CipherSuite::TLS_AES_256_GCM_SHA384,
-                                     CipherSuite::TLS_CHACHA20_POLY1305_SHA256,
-                                     CipherSuite::TLS_AES_128_GCM_SHA256])
+      def initialize(cipher_suites: DEFALT_CIPHER_SUITES)
         @cipher_suites = cipher_suites
         @length = 0
         @length = @cipher_suites.length * 2 unless @cipher_suites.nil?
       end
 
       def serialize
-        # TODO
+        binary = []
+        binary += [@length / (1 << 8), @length % (1 << 8)]
+        binary += @cipher_suites
+        binary
       end
 
       def self.deserialize(binary)
