0e23d937462ef4bf520967f8532233183481cc4a
thekuwayama <thekuwayama@gmail.com>
2019-02-13 01:52:07 +0900

    Add serialization test of CipherSuite

--- spec/cipher_suites_spec.rb
+++ spec/cipher_suites_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe CipherSuites do
-  context 'serialize' do
-    # TODO
+  context 'valid cipher suites' do
+    let(:cs) do
+      CipherSuites.new
+    end
+
+    it 'should generate default cipher suites' do
+      expect(cs.length).to eq 6
+      expect(cs.cipher_suites).to eq DEFALT_CIPHER_SUITES
+    end
+
+    it 'should serialize binary' do
+      expect(cs.serialize).to eq [0x00, 0x06] + DEFALT_CIPHER_SUITES
+    end
   end
 
   context 'deserialize' do
