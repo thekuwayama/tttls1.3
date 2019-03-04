e884e98e5e54d8eb628c361c91739f06382544b6
thekuwayama <thekuwayama@gmail.com>
2019-03-04 22:56:54 +0900

    Add CertificateVerify && mv SignatureScheme

---
+++ lib/tls13/message/certificate_verify.rb
@@ -0,0 +1,90 @@
+# encoding: ascii-8bit
+# frozen_string_literal: true
+
+require 'openssl'
+
+module TLS13
+  module Message
+    module SignatureScheme
+      # RSASSA-PKCS1-v1_5 algorithms
+      RSA_PKCS1_SHA256       = "\x04\x01"
+      RSA_PKCS1_SHA384       = "\x05\x01"
+      RSA_PKCS1_SHA512       = "\x06\x01"
+      # ECDSA algorithms
+      ECDSA_SECP256R1_SHA256 = "\x04\x03"
+      ECDSA_SECP384R1_SHA384 = "\x05\x03"
+      ECDSA_SECP521R1_SHA512 = "\x06\x03"
+      # RSASSA-PSS algorithms with public key OID rsaEncryption
+      RSA_PSS_RSAE_SHA256    = "\x08\x04"
+      RSA_PSS_RSAE_SHA384    = "\x08\x05"
+      RSA_PSS_RSAE_SHA512    = "\x08\x06"
+      # EdDSA algorithms
+      ED25519                = "\x08\x07"
+      ED448                  = "\x08\x08"
+      # RSASSA-PSS algorithms with public key OID RSASSA-PSS
+      RSA_PSS_PSS_SHA256     = "\x08\x09"
+      RSA_PSS_PSS_SHA384     = "\x08\x0a"
+      RSA_PSS_PSS_SHA512     = "\x08\x0b"
+      # Legacy algorithms
+      RSA_PKCS1_SHA1         = "\x02\x01"
+      ECDSA_SHA1             = "\x02\x03"
+      # Reserved Code Points
+      # private_use "\xfe\x00" ~ "\xff\xff"
+    end
+
+    class CertificateVerify
+      attr_reader   :msg_type
+      attr_accessor :signature_scheme
+      attr_accessor :signature
+
+      # @param signature_scheme [TLS13::Message::SignatureScheme]
+      # @param signature [String]
+      #
+      # @raise [RuntimeError]
+      def initialize(signature_scheme: nil,
+                     signature: '')
+        @msg_type = HandshakeType::CERTIFICATE_VERIFY
+        @signature_scheme = signature_scheme
+        @signature = signature || ''
+        raise 'invalid signature' if @signature.length > 2**16 - 1
+      end
+
+      # @return [Integer]
+      def length
+        4 + @signature.length
+      end
+
+      # @return [String]
+      def serialize
+        binary = ''
+        binary += @msg_type
+        binary += i2uint24(length)
+        binary += @signature_scheme
+        binary += i2uint16(@signature.length)
+        binary += @signature
+        binary
+      end
+
+      # @param binary [String]
+      #
+      # @raise [RuntimeError]
+      #
+      # @return [TLS13::Message::CertificateVerify]
+      def self.deserialize(binary)
+        raise 'invalid HandshakeType' \
+          unless binary[0] == HandshakeType::CERTIFICATE_VERIFY
+
+        msg_len = bin2i(binary.slice(1, 3))
+        signature_scheme = binary.slice(4, 2)
+        signature_len = bin2i(binary.slice(6, 2))
+        signature = binary.slice(4, signature_len)
+        raise 'malformed binary' \
+          unless binary.length == signature_len + 8 &&
+                 signature_len + 4 == msg_len
+
+        CertificateVerify.new(signature_scheme: signature_scheme,
+                              signature: signature)
+      end
+    end
+  end
+end
--- lib/tls13/message/extension/signature_algorithms.rb
+++ lib/tls13/message/extension/signature_algorithms.rb
@@ -4,33 +4,6 @@
 module TLS13
   module Message
     module Extension
-      module SignatureScheme
-        # RSASSA-PKCS1-v1_5 algorithms
-        RSA_PKCS1_SHA256       = "\x04\x01"
-        RSA_PKCS1_SHA384       = "\x05\x01"
-        RSA_PKCS1_SHA512       = "\x06\x01"
-        # ECDSA algorithms
-        ECDSA_SECP256R1_SHA256 = "\x04\x03"
-        ECDSA_SECP384R1_SHA384 = "\x05\x03"
-        ECDSA_SECP521R1_SHA512 = "\x06\x03"
-        # RSASSA-PSS algorithms with public key OID rsaEncryption
-        RSA_PSS_RSAE_SHA256    = "\x08\x04"
-        RSA_PSS_RSAE_SHA384    = "\x08\x05"
-        RSA_PSS_RSAE_SHA512    = "\x08\x06"
-        # EdDSA algorithms
-        ED25519                = "\x08\x07"
-        ED448                  = "\x08\x08"
-        # RSASSA-PSS algorithms with public key OID RSASSA-PSS
-        RSA_PSS_PSS_SHA256     = "\x08\x09"
-        RSA_PSS_PSS_SHA384     = "\x08\x0a"
-        RSA_PSS_PSS_SHA512     = "\x08\x0b"
-        # Legacy algorithms
-        RSA_PKCS1_SHA1         = "\x02\x01"
-        ECDSA_SHA1             = "\x02\x03"
-        # Reserved Code Points
-        # private_use "\xfe\x00" ~ "\xff\xff"
-      end
-
       class SignatureAlgorithms
         attr_accessor :extension_type # for signature_algorithms_cert
         attr_accessor :supported_signature_algorithms
