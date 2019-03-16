04ba71743f194f4e037a80bbc2f40780408f448d
thekuwayama <thekuwayama@gmail.com>
2019-03-16 18:47:13 +0900

    refactor hkdf_expand

--- lib/tls13/key_schedule.rb
+++ lib/tls13/key_schedule.rb
@@ -5,10 +5,13 @@ require 'openssl'
 
 module TLS13
   class KeySchedule
-    def initialize(psk: nil, digest:)
+    def initialize(psk: nil, shared_secret:, digest:)
       @digest = digest
       @hash_len = hash_len(@digest)
       @psk = psk || "\x00" * @hash_len
+      @shared_secret = shared_secret # TODO: check shared_secret.length
+      # @write_key_len
+      # @iv_len
     end
 
     # @return [String]
@@ -17,81 +20,81 @@ module TLS13
     end
 
     # @return [String]
+    def early_secret
+      hkdf_extract(@psk, early_salt)
+    end
+
+    # @return [String]
     def client_early_traffic_secret
-      derive_secret(@psk, early_salt, 'c e traffic', '')
+      derive_secret(early_secret, 'c e traffic', '')
     end
 
     # @return [String]
     def early_exporter_master_secret
-      derive_secret(@psk, early_salt, 'e exp master', '')
+      derive_secret(early_secret, 'e exp master', '')
     end
 
     # @return [String]
     def handshake_salt
-      derive_secret(@psk, early_salt, 'derived', '')
+      derive_secret(early_secret, 'derived', '')
     end
 
-    # @params shared_secret [String] (EC)DHE
+    # @return [String]
+    def handshake_secret
+      hkdf_extract(@shared_secret, handshake_salt)
+    end
+
     # @params messages [String] serialized ClientHello and ServerHello
     #
     # @return [String]
-    def client_handshake_traffic_secret(shared_secret, messages)
-      derive_secret(shared_secret, handshake_salt, 'c hs traffic', messages)
+    def client_handshake_traffic_secret(messages)
+      derive_secret(handshake_secret, 'c hs traffic', messages)
     end
 
-    # @params shared_secret [String] (EC)DHE
     # @params messages [String] serialized ClientHello and ServerHello
     #
     # @return [String]
-    def server_handshake_traffic_secret(shared_secret, messages)
-      derive_secret(shared_secret, handshake_salt, 's hs traffic', messages)
+    def server_handshake_traffic_secret(messages)
+      derive_secret(handshake_secret, 's hs traffic', messages)
     end
 
-    # @params shared_secret [String] (EC)DHE
-    #
     # @return [String]
-    def master_salt(shared_secret)
-      derive_secret(shared_secret, handshake_salt, 'derived', '')
+    def master_salt
+      derive_secret(handshake_secret, 'derived', '')
     end
 
-    # @params shared_secret [String] (EC)DHE
+    # @return [String]
+    def master_secret
+      ikm = "\x00" * @hash_len
+      hkdf_extract(ikm, master_salt)
+    end
+
     # @params messages [String] serialized ClientHello...server Finished
     #
     # @return [String]
-    def client_application_traffic_secret(shared_secret, messages)
-      ikm = "\x00" * @hash_len
-      salt = master_salt(shared_secret)
-      derive_secret(ikm, salt, 'c ap traffic', messages)
+    def client_application_traffic_secret(messages)
+      derive_secret(master_secret, 'c ap traffic', messages)
     end
 
-    # @params shared_secret [String] (EC)DHE
     # @params messages [String] serialized ClientHello...server Finished
     #
     # @return [String]
-    def server_application_traffic_secret(shared_secret, messages)
-      ikm = "\x00" * @hash_len
-      salt = master_salt(shared_secret)
-      derive_secret(ikm, salt, 's ap traffic', messages)
+    def server_application_traffic_secret(messages)
+      derive_secret(master_secret, 's ap traffic', messages)
     end
 
-    # @params shared_secret [String] (EC)DHE
     # @params messages [String] serialized ClientHello...server Finished
     #
     # @return [String]
-    def exporter_master_secret(shared_secret, messages)
-      ikm = "\x00" * @hash_len
-      salt = master_salt(shared_secret)
-      derive_secret(ikm, salt, 'exp master', messages)
+    def exporter_master_secret(messages)
+      derive_secret(master_secret, 'exp master', messages)
     end
 
-    # @params shared_secret [String] (EC)DHE
     # @params messages [String] serialized ClientHello...client Finished
     #
     # @return [String]
-    def resumption_master_secret(shared_secret, messages)
-      ikm = "\x00" * @hash_len
-      salt = master_salt(shared_secret)
-      derive_secret(ikm, salt, 'res master', messages)
+    def resumption_master_secret(messages)
+      derive_secret(master_secret, 'res master', messages)
     end
 
     # @params messages [String]
@@ -110,30 +113,46 @@ module TLS13
       OpenSSL::HMAC.digest(@digest, salt, ikm)
     end
 
-    # @params ikm [String]
-    # @params salt [String]
+    # @params secret [String]
     # @params label [String]
     # @params context [String]
     #
     # @return [String]
-    def hkdf_expand_label(ikm, salt, label, context)
+    def hkdf_expand_label(secret, label, context)
       binary = i2uint16(@hash_len)
       binary += uint8_length_prefix('tls13 ' + label)
       binary += uint8_length_prefix(context)
-      hash = OpenSSL::Digest.new(@digest)
-      OpenSSL::KDF.hkdf(ikm, salt: salt, info: binary, length: @hash_len,
-                             hash: hash)
+      hkdf_expand(secret, binary, @hash_len)
     end
 
-    # @params ikm [String]
-    # @params salt [String]
+    # @params secret [String]
+    # @params info [String]
+    # @params length [Integer]
+    #
+    # @raise [RuntimeError]
+    #
+    # @params [String]
+    def hkdf_expand(secret, info, length)
+      raise 'too long length' if length > 255 * @hash_len
+
+      n = (length.to_f / @hash_len).ceil
+      okm = ''
+      t = ''
+      (1..n).each do |i|
+        t = OpenSSL::HMAC.digest(@digest, secret, t + info + i.chr)
+        okm += t
+      end
+      okm[0...length]
+    end
+
+    # @params secret [String]
     # @params label [String]
     # @params messages [String]
     #
     # @return [String]
-    def derive_secret(ikm, salt, label, messages)
+    def derive_secret(secret, label, messages)
       context = transcript_hash(messages)
-      hkdf_expand_label(ikm, salt, label, context)
+      hkdf_expand_label(secret, label, context)
     end
   end
 end
--- spec/key_schedule_spec.rb
+++ spec/key_schedule_spec.rb
@@ -6,11 +6,7 @@ require 'spec_helper'
 RSpec.describe KeySchedule do
   context 'key_schedule' do
     let(:key_schedule) do
-      KeySchedule.new(digest: 'SHA256')
-    end
-
-    let(:ss) do
-      TESTBINARY_SHARED_SECRET
+      KeySchedule.new(shared_secret: TESTBINARY_SHARED_SECRET, digest: 'SHA256')
     end
 
     let(:ch_sh) do
@@ -32,17 +28,17 @@ RSpec.describe KeySchedule do
     end
 
     it 'should generate secret' do
-      expect(key_schedule.client_handshake_traffic_secret(ss, ch_sh))
+      expect(key_schedule.client_handshake_traffic_secret(ch_sh))
         .to eq TESTBINARY_C_HS_TRAFFIC
-      expect(key_schedule.server_handshake_traffic_secret(ss, ch_sh))
+      expect(key_schedule.server_handshake_traffic_secret(ch_sh))
         .to eq TESTBINARY_S_HS_TRAFFIC
-      expect(key_schedule.client_application_traffic_secret(ss, ch_sf))
+      expect(key_schedule.client_application_traffic_secret(ch_sf))
         .to eq TESTBINARY_C_AP_TRAFFIC
-      expect(key_schedule.server_application_traffic_secret(ss, ch_sf))
+      expect(key_schedule.server_application_traffic_secret(ch_sf))
         .to eq TESTBINARY_S_AP_TRAFFIC
-      expect(key_schedule.exporter_master_secret(ss, ch_sf))
+      expect(key_schedule.exporter_master_secret(ch_sf))
         .to eq TESTBINARY_EXP_MASTER
-      expect(key_schedule.resumption_master_secret(ss, ch_cf))
+      expect(key_schedule.resumption_master_secret(ch_cf))
         .to eq TESTBINARY_RES_MASTER
     end
   end
