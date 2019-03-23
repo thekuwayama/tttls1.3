76f8d10df26b4db54de409a9f8932e3123a4ee4a
thekuwayama <thekuwayama@gmail.com>
2019-03-23 10:23:18 +0900

    modify localhost port

--- example/client.rb
+++ example/client.rb
@@ -2,7 +2,7 @@
 
 require_relative 'helper'
 
-hostname, port = (ARGV[0] || 'localhost:443').split(':')
+hostname, port = (ARGV[0] || 'localhost:4433').split(':')
 socket = TCPSocket.new(hostname, port)
 client = TLS13::Client.new(socket)
 client.hostname = hostname
