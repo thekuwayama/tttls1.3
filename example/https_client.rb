aa0c282d6b66236f0f7dda3134806f73b68cb965
thekuwayama <thekuwayama@gmail.com>
2019-03-28 18:46:17 +0900

    update example/https_client.rb

--- example/https_client.rb
+++ example/https_client.rb
@@ -7,3 +7,12 @@ socket = TCPSocket.new(hostname, port)
 client = TLS13::Client.new(socket)
 client.hostname = hostname
 client.connect
+http_get = <<~BIN
+  GET / HTTP/1.1\r
+  Host: #{hostname}\r
+  User-Agent: https_client\r
+  Accept: */*\r
+  \r
+BIN
+client.write(http_get)
+puts client.read
