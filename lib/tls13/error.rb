09a6b804eb2fbc9758e197fabdf31445da8acbbf
thekuwayama <thekuwayama@gmail.com>
2019-04-01 00:45:13 +0900

    add TLS13::Error

---
+++ lib/tls13/error.rb
@@ -0,0 +1,19 @@
+# encoding: ascii-8bit
+# frozen_string_literal: true
+
+module TLS13
+  module Error
+    # Generic error, common for all classes under TLS13::Error module.
+    class Error < StandardError; end
+
+    # Raised if configure is invalid.
+    class ConfigError < Error; end
+
+    # Raised on invalid connection processing.
+    class InternalError < Error; end
+
+    # Raised on received Alert message or invalid message.
+    # Terminated the connection, so you *cannot* recover from this exception.
+    class TLSError < Error; end
+  end
+end
