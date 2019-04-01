e731dea63f48defb63faa02dce3fdb633f0c0f89
thekuwayama <thekuwayama@gmail.com>
2019-04-02 05:41:14 +0900

    modify EncryptedExtensions; raise [TLS13::Error::InternalError, TLSError]

--- lib/tls13/message/encrypted_extensions.rb
+++ lib/tls13/message/encrypted_extensions.rb
@@ -23,19 +23,22 @@ module TLS13
 
       # @param binary [String]
       #
-      # @raise [RuntimeError]
+      # @raise [TLS13::Error::InternalError, TLSError]
       #
       # @return [TLS13::Message::EncryptedExtensions]
       def self.deserialize(binary)
-        raise 'invalid HandshakeType' \
+        raise Error::InternalError if binary.nil?
+        raise Error::TLSError, 'decode_error' if binary.length < 6
+        raise Error::InternalError \
           unless binary[0] == HandshakeType::ENCRYPTED_EXTENSIONS
 
         ee_len = Convert.bin2i(binary.slice(1, 3))
         exs_len = Convert.bin2i(binary.slice(4, 2))
-        raise 'malformed binary' unless ee_len == exs_len + 2
-
         extensions = Extensions.deserialize(binary.slice(6, exs_len),
                                             HandshakeType::ENCRYPTED_EXTENSIONS)
+        raise Error::TLSError, 'decode_error' \
+          unless exs_len + 2 == ee_len && exs_len + 6 == binary.length
+
         EncryptedExtensions.new(extensions)
       end
     end
