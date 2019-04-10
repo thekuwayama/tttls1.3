f4eb5a104ac0ce699cb5eec6ab3ec893e56f6f2d
thekuwayama <thekuwayama@gmail.com>
2019-04-10 19:22:16 +0900

    update example/https_client.rb

--- example/https_client.rb
+++ example/https_client.rb
@@ -15,4 +15,18 @@ http_get = <<~BIN
   \r
 BIN
 client.write(http_get)
-puts client.read
+
+# status line, header
+buffer = ''
+buffer += client.read until buffer.include?("\r\n\r\n")
+print header = buffer.split("\r\n\r\n").first
+# header; Content-Length
+cl_line = header.split("\r\n").find { |s| s.match(/Content-Length:/i) }
+cl = cl_line.split(':').last.to_i
+
+# body
+print buffer = buffer.split("\r\n\r\n")[1..].join
+while buffer.length < cl
+  print s = client.read
+  buffer += s
+end
