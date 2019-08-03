7e6d0344aab02158f4165aac0dda4ae93870b714
thekuwayama <thekuwayama@gmail.com>
2019-08-03 12:53:07 +0900

    interop with rspec-retry

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ group :test do
   gem 'pry'
   gem 'pry-byebug'
   gem 'rspec', '3.8.0'
+  gem 'rspec-retry', '0.6.1'
   gem 'rubocop', '0.67.2'
 end
 
--- interop/helper.rb
+++ interop/helper.rb
@@ -1,10 +1,9 @@
-# encoding: ascii-8bit
 # frozen_string_literal: true
-
-RSpec.configure(&:disable_monkey_patching!)
 
 # rubocop: disable Style/MixinUsage
 require 'tttls1.3'
+require 'rspec/retry'
+
 include TTTLS13
 include TTTLS13::CipherSuite
 include TTTLS13::SignatureScheme
@@ -12,6 +11,16 @@ include TTTLS13::Message::Extension
 include TTTLS13::Error
 # rubocop: enable Style/MixinUsage
 
+INTERVAL = 0.2
+
 def wait_to_listen(port)
-  sleep(0.2) while `lsof -ni :#{port}`.empty?
+  sleep(INTERVAL) while `lsof -ni :#{port}`.empty?
+end
+
+RSpec.configure do |config|
+  config.verbose_retry = true
+  config.default_retry_count = 2
+  config.display_try_failure_messages = true
+  config.default_sleep_interval = INTERVAL * 2
+  config.clear_lets_on_failure = false
 end
