35973605db8ba82f18b1f9db625dd8d6f36d545f
thekuwayama <thekuwayama@gmail.com>
2020-01-13 19:05:51 +0900

    modify how to decode SubjectPublicKeyInfo

--- lib/tttls1.3/connection.rb
+++ lib/tttls1.3/connection.rb
@@ -514,22 +514,22 @@ module TTTLS13
     def do_select_signature_algorithms(signature_algorithms, crt)
       spki = OpenSSL::Netscape::SPKI.new
       spki.public_key = crt.public_key
-      oid_str = spki.to_text.split("\n")
-                    .find { |l| l.include?('Public Key Algorithm:') }
+      pka = OpenSSL::ASN1.decode(spki.to_der)
+                         .value.first.value.first.value.first.value.first.value
       signature_algorithms.select do |sa|
         case sa
         when SignatureScheme::ECDSA_SECP256R1_SHA256,
              SignatureScheme::ECDSA_SECP384R1_SHA384,
              SignatureScheme::ECDSA_SECP521R1_SHA512
-          oid_str.include?('id-ecPublicKey')
+          pka == 'id-ecPublicKey'
         when SignatureScheme::RSA_PSS_PSS_SHA256,
              SignatureScheme::RSA_PSS_PSS_SHA384,
              SignatureScheme::RSA_PSS_PSS_SHA512
-          oid_str.include?('rsassaPss')
+          pka == 'rsassaPss'
         when SignatureScheme::RSA_PSS_RSAE_SHA256,
              SignatureScheme::RSA_PSS_RSAE_SHA384,
              SignatureScheme::RSA_PSS_RSAE_SHA512
-          oid_str.include?('rsaEncryption')
+          pka == 'rsaEncryption'
         else
           # RSASSA-PKCS1-v1_5 algorithms refer solely to signatures which appear
           # in certificates and are not defined for use in signed TLS handshake
