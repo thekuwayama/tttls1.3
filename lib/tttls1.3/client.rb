63690ab8653ecc368da580d061a342caf6b712e2
thekuwayama <thekuwayama@gmail.com>
2022-08-18 09:17:59 +0900

    first implements

--- lib/tttls1.3/client.rb
+++ lib/tttls1.3/client.rb
@@ -68,7 +68,8 @@ module TTTLS13
     process_certificate_status: nil,
     compress_certificate_algorithms: DEFALUT_CH_COMPRESS_CERTIFICATE_ALGORITHMS,
     compatibility_mode: true,
-    loglevel: Logger::WARN
+    loglevel: Logger::WARN,
+    sslkeylogfile: nil
   }.freeze
   private_constant :DEFAULT_CLIENT_SETTINGS
 
@@ -151,6 +152,8 @@ module TTTLS13
       hs_wcipher = nil # TTTLS13::Cryptograph::$Object
       hs_rcipher = nil # TTTLS13::Cryptograph::$Object
       e_wcipher = nil # TTTLS13::Cryptograph::$Object
+      sslkeylogfile = SslKeyLogFile::Writer.new(@settings[:sslkeylogfile]) \
+        unless @settings[:sslkeylogfile].nil?
 
       @state = ClientState::START
       loop do
@@ -169,6 +172,10 @@ module TTTLS13
               key_schedule.early_data_write_key,
               key_schedule.early_data_write_iv
             )
+            sslkeylogfile.write_client_early_traffic_secret(
+              transcript[CH].first.random,
+              key_schedule.client_early_traffic_secret
+            ) unless sslkeylogfile.nil?
             send_early_data(e_wcipher)
           end
 
@@ -276,11 +283,19 @@ module TTTLS13
             key_schedule.client_handshake_write_key,
             key_schedule.client_handshake_write_iv
           )
+          sslkeylogfile.write_client_handshake_traffic_secret(
+            transcript[CH].first.random,
+            key_schedule.client_handshake_traffic_secret
+          ) unless sslkeylogfile.nil?
           hs_rcipher = gen_cipher(
             @cipher_suite,
             key_schedule.server_handshake_write_key,
             key_schedule.server_handshake_write_iv
           )
+          sslkeylogfile.write_client_handshake_traffic_secret(
+            transcript[CH].first.random,
+            key_schedule.server_handshake_traffic_secret
+          ) unless sslkeylogfile.nil?
           @state = ClientState::WAIT_EE
         when ClientState::WAIT_EE
           logger.debug('ClientState::WAIT_EE')
@@ -388,11 +403,19 @@ module TTTLS13
             key_schedule.client_application_write_key,
             key_schedule.client_application_write_iv
           )
+          sslkeylogfile.write_client_traffic_secret_0(
+            transcript[CH].first.random,
+            key_schedule.client_application_traffic_secret
+          ) unless sslkeylogfile.nil?
           @ap_rcipher = gen_cipher(
             @cipher_suite,
             key_schedule.server_application_write_key,
             key_schedule.server_application_write_iv
           )
+          sslkeylogfile.write_server_traffic_secret_0(
+            transcript[CH].first.random,
+            key_schedule.server_application_traffic_secret
+          ) unless sslkeylogfile.nil?
           @exporter_master_secret = key_schedule.exporter_master_secret
           @resumption_master_secret = key_schedule.resumption_master_secret
           @state = ClientState::CONNECTED
@@ -402,6 +425,8 @@ module TTTLS13
           break
         end
       end
+
+      sslkeylogfile.close
     end
     # rubocop: enable Metrics/AbcSize
     # rubocop: enable Metrics/BlockLength
---
+++ lib/tttls1.3/sslkeylogfile.rb
@@ -0,0 +1,64 @@
+# encoding: ascii-8bit
+# frozen_string_literal: true
+
+module TTTLS13
+  module SslKeyLogFile
+    module Label
+      CLIENT_EARLY_TRAFFIC_SECRET     = 'CLIENT_EARLY_TRAFFIC_SECRET'
+      CLIENT_HANDSHAKE_TRAFFIC_SECRET = 'CLIENT_HANDSHAKE_TRAFFIC_SECRET'
+      SERVER_HANDSHAKE_TRAFFIC_SECRET = 'SERVER_HANDSHAKE_TRAFFIC_SECRET'
+      CLIENT_TRAFFIC_SECRET_0         = 'CLIENT_TRAFFIC_SECRET_0'
+      SERVER_TRAFFIC_SECRET_0         = 'SERVER_TRAFFIC_SECRET_0'
+    end
+
+    class Writer
+      # @param path [String]
+      def initialize(path)
+        @file = File.new(path, 'a+')
+      end
+
+      # @param client_random [String]
+      # @param secret [String]
+      def write_client_early_traffic_secret(client_random, secret)
+        write_key_log(Label::CLIENT_EARLY_TRAFFIC_SECRET, client_random, secret)
+      end
+
+      # @param client_random [String]
+      # @param secret [String]
+      def write_client_handshake_traffic_secret(client_random, secret)
+        write_key_log(Label::CLIENT_HANDSHAKE_TRAFFIC_SECRET, client_random, secret)
+      end
+
+      # @param client_random [String]
+      # @param secret [String]
+      def write_server_handshake_traffic_secret(client_random, secret)
+        write_key_log(Label::SERVER_HANDSHAKE_TRAFFIC_SECRET, client_random, secret)
+      end
+
+      # @param client_random [String]
+      # @param secret [String]
+      def write_client_traffic_secret_0(client_random, secret)
+        write_key_log(Label::CLIENT_TRAFFIC_SECRET_0, client_random, secret)
+      end
+
+      # @param client_random [String]
+      # @param secret [String]
+      def write_server_traffic_secret_0(client_random, secret)
+        write_key_log(Label::SERVER_TRAFFIC_SECRET_0, client_random, secret)
+      end
+
+      def close
+        @file.close
+      end
+
+      private
+
+      # @param label [TTTLS13::SslKeyLogFile::Label]
+      # @param client_random [String]
+      # @param secret [String]
+      def write_key_log(label, client_random, secret)
+        @file.print("#{label} #{client_random.unpack1('H*')} #{secret.unpack1('H*')}\n")
+      end
+    end
+  end
+end
