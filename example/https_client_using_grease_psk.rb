fe2e8aed4771900fde04639aebfdf04c08c0b6bf
thekuwayama <thekuwayama@gmail.com>
2025-05-11 14:34:27 +0900

    fix: example/https_client_using_grease_psk.rb using parse_echconfigs_pem & resolve_echconfig

--- example/https_client_using_grease_psk.rb
+++ example/https_client_using_grease_psk.rb
@@ -8,15 +8,16 @@ HpkeSymmetricCipherSuite = \
 uri = URI.parse(ARGV[0] || 'https://localhost:4433')
 ca_file = __dir__ + '/../tmp/ca.crt'
 req = simple_http_request(uri.host, uri.path)
+ech_config = if ARGV.length > 1
+               parse_echconfigs_pem(File.open(ARGV[1]).read).first
+             else
+               resolve_echconfig(uri.host)
+             end
 
-rr = Resolv::DNS.new.getresources(
-  uri.host,
-  Resolv::DNS::Resource::IN::HTTPS
-)
 settings_2nd = {
   ca_file: File.exist?(ca_file) ? ca_file : nil,
   alpn: ['http/1.1'],
-  ech_config: rr.first.svc_params['ech'].echconfiglist.first,
+  ech_config:,
   ech_hpke_cipher_suites:
     TTTLS13::STANDARD_CLIENT_ECH_HPKE_SYMMETRIC_CIPHER_SUITES,
   sslkeylogfile: '/tmp/sslkeylogfile.log'
@@ -35,7 +36,7 @@ settings_1st = {
   ca_file: File.exist?(ca_file) ? ca_file : nil,
   alpn: ['http/1.1'],
   process_new_session_ticket:,
-  ech_config: rr.first.svc_params['ech'].echconfiglist.first,
+  ech_config:,
   ech_hpke_cipher_suites:
     TTTLS13::STANDARD_CLIENT_ECH_HPKE_SYMMETRIC_CIPHER_SUITES,
   sslkeylogfile: '/tmp/sslkeylogfile.log'
